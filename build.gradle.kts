c081d7477776d2fb8b0f3fb9973124c3bb079dd2
ravisingal <62086374+ravisingal@users.noreply.github.com>
2021-04-08 12:51:17 +0530

    publish version settings plugin to hypertrace artifactory (#9)
    * publich version settings plugin to hypertrace artifactory
    
    * update publish workflow
    
    * update version-settings plugin version
    
    * update artifactory url to maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.hypertrace.gradle.publishing.License.APACHE_2_0
 
 plugins {
-  id("org.hypertrace.publish-plugin") version "0.3.1"
-  id("org.hypertrace.ci-utils-plugin") version "0.1.1"
+  id("org.hypertrace.publish-plugin") version "1.0.2"
+  id("org.hypertrace.ci-utils-plugin") version "0.2.0"
   `java-gradle-plugin`
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ pluginManagement {
     mavenLocal()
     gradlePluginPortal()
     maven {
-      url = uri("https://dl.bintray.com/hypertrace/maven")
+      url = uri("https://hypertrace.jfrog.io/artifactory/maven")
     }
   }
 }
 
 plugins {
-  id("org.hypertrace.version-settings") version "0.1.1"
+  id("org.hypertrace.version-settings") version "0.1.6"
 }
 
 rootProject.name = "hypertrace-gradle-version-settings-plugin"

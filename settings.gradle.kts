564a69fa3fadafe028be6acd0868a9b6b2c7d7fc
Ravi Singal <ravi.singal@traceable.ai>
2021-04-08 10:35:04 +0530

    publich version settings plugin to hypertrace artifactory

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ pluginManagement {
     mavenLocal()
     gradlePluginPortal()
     maven {
-      url = uri("https://dl.bintray.com/hypertrace/maven")
+      url = uri("https://hypertrace.jfrog.io/artifactory/gradle")
     }
   }
 }
 
 plugins {
-  id("org.hypertrace.version-settings") version "0.1.1"
+  id("org.hypertrace.version-settings") version "0.6.0"
 }
 
 rootProject.name = "hypertrace-gradle-version-settings-plugin"
